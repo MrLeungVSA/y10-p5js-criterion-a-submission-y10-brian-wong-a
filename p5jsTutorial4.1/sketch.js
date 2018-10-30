9a476df2a9317d9cca6c7131482bcb824150d42c
SaltySalt11242 <39963329+SaltySalt11242@users.noreply.github.com>
2018-10-30 10:38:36 +0800

    Add files via upload

---
+++ p5jsTutorial4.1/sketch.js
@@ -0,0 +1,28 @@
+// declare bgm variable
+var bgm;
+
+function preload()
+{
+	// load sound file into variable bgm
+	bgm = loadSound("https://bleungwpg.github.io/p5jsTutorial4.1/sounds/Undertale-Megalovania.mp3");
+
+}
+
+function setup()
+{
+	createCanvas(800,600);
+
+	// play music during setup so that it doesn't loop infinitely
+	bgm.play();
+}
+
+function draw()
+{
+	textSize(12);
+
+	background(125,125,125)
+
+	text("Credit for music goes to Benjamin TISSOT from www.bensound.com",10,40);
+
+
+}
